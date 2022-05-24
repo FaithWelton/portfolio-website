e68dcc6328e0ba12bea32d272bc327f7f309c801
FaithMadore <faithmadore@hotmail.com>
2022-05-24 12:27:38 -0400

    Update personal info

--- components/constants.js
+++ components/constants.js
@@ -1,18 +1,18 @@
 export const siteTitle = 'Faith\'s Portfolio'
-export const footer = <strong>This website was made by Faith Madore 2021</strong>
+export const footer = <strong>This website was made by Faith Welton</strong>
 
 export const name = 'Hi, I\'m Faith!'
 export const email = 'mailto:faithmadore@hotmail.com'
-export const linkedin = 'https://www.linkedin.com/in/faithmadore/'
-export const about = 'I am currently headed into my third and final year of Conestoga College\'s SET program, where I\'ve worked on several projects using different technologies. You can find more information about these on my projects page. Through my education I have found a love for UI design and other front-end development. I have a strong education background, love to learn new things and see each new project as another puzzle to solve.'
-export const education = 'Conestoga College - Software Engineering Technology (September 2019 - present)'
+export const linkedin = 'https://www.linkedin.com/in/faithwelton/'
+export const about = 'I am a fresh graduate from Conestoga College\'s SET program, where I\'ve worked on several projects using different technologies. You can find more information about these on my projects page. Through my education I have found a love for UI design and other front-end development. I have a strong education background, love to learn new things and see each new project as another puzzle to solve.'
+export const education = 'Conestoga College - Software Engineering Technology (2019 - 2022)'
 
 /* Projects */
 export const projects = [
     {
-        title: "Deal or No Deal",
-        content: "Simple console game made in C++ where the player must choose from 26 briefcases containing different unknown amounts of money. The remaining briefcases will be opened while the player receives offers to quit before their briefcase is revealed.",
-        git: "https://github.com/FaithMadore/DoND"
+        title: "SET Capstone Project - dForms",
+        content: "Digital Form Creation - Designer application and accompanying Player application",
+        git: "https://github.com/FaithWelton/SET2022Capstone"
     },
     {
         title: "TCPIP Chat App",
@@ -20,9 +20,9 @@ export const projects = [
         git: "https://github.com/FaithMadore/SimpleChatApp"
     },
     {
-        title: "TCP Web Server",
-        content: "Simple TCP web server made in C# with logging and client-server communications",
-        git: "https://github.com/FaithMadore/WDD-A6"
+        title: "Deal or No Deal",
+        content: "Simple console game made in C++ where the player must choose from 26 briefcases containing different unknown amounts of money. The remaining briefcases will be opened while the player receives offers to quit before their briefcase is revealed.",
+        git: "https://github.com/FaithMadore/DoND"
     },
     {
         title: "Portfolio Website",
@@ -34,7 +34,7 @@ export const projects = [
 /* Skills */
 export const skills = [
     {
-        content: "Programming Languages include: JavaScript, C++, C#, HTML, CSS"
+        content: "JavaScript, HTML + CSS, C#, Python, React, NodeJs, C/C++, Azure Devops, Agile Ceremonies"
     },
     {
         content: "Strong organization, communication & time management"
